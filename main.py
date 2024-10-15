c8ca4cf901f47f77e194c400c71d25939372530c
Preston Swigart <prestonlswigart@gmail.com>
2024-10-15 13:31:49 -0400

    im committing the working game now so i dont accidentally ruin it later and have no backups

--- main.py
+++ main.py
@@ -12,103 +12,99 @@ speed = 4
 
 
 
-# extracting game items and characters form the resource.png image.
+# extracting game items and characters from the resource.png image.
 player_init = Image.open("resources.png").crop((77,5,163,96)).convert("RGBA")
 player_init = player_init.resize(list(map(lambda x:x//2 , player_init.size)))
-
 player_frame_1 = Image.open("resources.png").crop((1679,2,1765,95)).convert("RGBA")
 player_frame_1 = player_frame_1.resize(list(map(lambda x:x//2 , player_frame_1.size)))
-
 player_frame_2 = Image.open("resources.png").crop((1767,2,1853,95)).convert("RGBA")
 player_frame_2 = player_frame_2.resize(list(map(lambda x:x//2 , player_frame_2.size)))
-
 player_frame_3 = Image.open("resources.png").crop((1855,2,1941,95)).convert("RGBA")
 player_frame_3 = player_frame_3.resize(list(map(lambda x:x//2 , player_frame_3.size)))
-
 player_frame_31 = Image.open("resources.png").crop((1943,2,2029,95)).convert("RGBA")
 player_frame_31 = player_frame_31.resize(list(map(lambda x:x//2 , player_frame_31.size)))
-
 player_frame_4 = Image.open("resources.png").crop((2030,2,2117,95)).convert("RGBA")
 player_frame_4 = player_frame_4.resize(list(map(lambda x:x//2 , player_frame_4.size)))
-
 player_frame_5 = Image.open("resources.png").crop((2207,2,2323,95)).convert("RGBA")
 player_frame_5 = player_frame_5.resize(list(map(lambda x:x//2 , player_frame_5.size)))
-
 player_frame_6 = Image.open("resources.png").crop((2324,2,2441,95)).convert("RGBA")
 player_frame_6 = player_frame_6.resize(list(map(lambda x:x//2 , player_frame_6.size)))
-
 cloud = Image.open("resources.png").crop((166,2,257,29)).convert("RGBA")
 cloud = cloud.resize(list(map(lambda x:x//2 , cloud.size)))
-
 ground = Image.open("resources.png").crop((2,102,2401,127)).convert("RGBA")
 ground = ground.resize(list(map(lambda x:x//2 , ground.size)))
-
 obstacle1 = Image.open("resources.png").crop((446,2,479,71)).convert("RGBA")
 obstacle1 = obstacle1.resize(list(map(lambda x:x//2 , obstacle1.size)))
-
 obstacle2 = Image.open("resources.png").crop((446,2,547,71)).convert("RGBA")
 obstacle2 = obstacle2.resize(list(map(lambda x:x//2 , obstacle2.size)))
-
 obstacle3 = Image.open("resources.png").crop((446,2,581,71)).convert("RGBA")
 obstacle3 = obstacle3.resize(list(map(lambda x:x//2 , obstacle3.size)))
-
 obstacle4 = Image.open("resources.png").crop((653,2,701,101)).convert("RGBA")
 obstacle4 = obstacle4.resize(list(map(lambda x:x//2 , obstacle4.size)))
-
 obstacle5 = Image.open("resources.png").crop((653,2,701,101)).convert("RGBA")
 obstacle5 = obstacle5.resize(list(map(lambda x:x//2 , obstacle5.size)))
-
-obstacle5 = Image.open("resources.png").crop((653,2,749,101)).convert("RGBA")
-obstacle5 = obstacle5.resize(list(map(lambda x:x//2 , obstacle5.size)))
-
 obstacle6 = Image.open("resources.png").crop((851,2,950,101)).convert("RGBA")
 obstacle6 = obstacle6.resize(list(map(lambda x:x//2 , obstacle6.size)))
 
-speed_identifier = lambda x: 2 if x >= 30 else 8 if x < 8 else 5
-cust_speed = speed_identifier(speed)
+
+cust_speed = 8
 running = cycle([player_frame_3]*cust_speed+[player_frame_31]*cust_speed)
 crouch = cycle([player_frame_5]*cust_speed+ [player_frame_6]*cust_speed)
 crouch_scope = [player_frame_5]+[player_frame_6]
 obstacles = [obstacle1,obstacle2, obstacle3,obstacle4,obstacle5,obstacle6]
 
-
-gameDisplay = pygame.display.set_mode((600,200))
+gameDisplay = pygame.display.set_mode((600, 200))
 pygame.display.set_caption('Dino Game')
 clock = pygame.time.Clock()
-state = player_frame_1
-crashed = False
-lock = False
-score = 0
-tscore = 0
-bg = (0, 150)
-bg1 = (600,150)
 start = False
-height = 110
-jumping = False
-slow_motion = False
-c1 = (rnd(30, 600), rnd(0, 100))
-c2 = (rnd(50,600), rnd(0, 100))
-c3 = (rnd(30,700), rnd(0, 100))
-c4 = (rnd(30,600),rnd(0, 100))
-obs1 = (rnd(600, 600+500), 130)
-obs2 = (rnd(600+100+500, 1200+500), 130)
-obs3 = (rnd(1700, 2000), 130)
-obast1 = choice(obstacles)
-if obast1 in [obstacle4, obstacle5, obstacle6]:obs1 = (obs1[0], 115)
-obast2 = choice(obstacles)
-if obast2 in [obstacle4, obstacle5, obstacle6]:obs2 = (obs2[0], 115)
-obast3 = choice(obstacles)
-if obast3 in [obstacle4, obstacle5, obstacle6]:obs3 = (obs3[0], 115)
+crashed = True
+Running = True
+frameCounter = 0
+maxScore = -1
+score = 0
+#run the game
+while Running:
+    #this should reset the game
+    score = int(frameCounter/12)
+    if(crashed):
+        if(score > maxScore):
+            print(score)
+            maxScore = score
+        frameCounter = 0
+        score = 0
+        state = player_frame_1
+        lock = False
+        bg = (0, 150)
+        bg1 = (600, 150)
+        height = 110
+        jumping = False
+        slow_motion = False
+        c1 = (rnd(30, 600), rnd(0, 100))
+        c2 = (rnd(50, 600), rnd(0, 100))
+        c3 = (rnd(30, 700), rnd(0, 100))
+        c4 = (rnd(30, 600), rnd(0, 100))
+        obs1 = (rnd(600, 600 + 500), 130)
+        obs2 = (rnd(600 + 100 + 500, 1200 + 500), 130)
+        obs3 = (rnd(1700, 2000), 130)
+        obast1 = choice(obstacles)
+        if obast1 in [obstacle4, obstacle5, obstacle6]: obs1 = (obs1[0], 115)
+        obast2 = choice(obstacles)
+        if obast2 in [obstacle4, obstacle5, obstacle6]: obs2 = (obs2[0], 115)
+        obast3 = choice(obstacles)
+        if obast3 in [obstacle4, obstacle5, obstacle6]: obs3 = (obs3[0], 115)
+        crashed = not crashed
+        start = not start
 
-while not crashed:
     gameDisplay.fill((255,255,255))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
             start = False
+            Running = False
             print("crashed")
         if event.type==pygame.KEYDOWN:
-            start = True
+            if(not crashed):
+                start = True
             if event.key == pygame.K_DOWN:
                 slow_motion = True
                 state = crouch
@@ -119,6 +115,7 @@ while not crashed:
             slow_motion = False
             if event.key == pygame.K_DOWN:
                 state = running
+
     player = state if type(state) != cycle else next(state)
     gameDisplay.blit(pygame.image.fromstring(cloud.tobytes(), cloud.size, 'RGBA'), c1)
     gameDisplay.blit(pygame.image.fromstring(cloud.tobytes(), cloud.size, 'RGBA'), c2)
@@ -160,12 +157,13 @@ while not crashed:
         obast2 = choice(obstacles)
         if obast2 in [obstacle4, obstacle5, obstacle6]:obs2 = (obs2[0], 115)
     if obs3[0]<=-50:
-        obs3 = (rnd(1700, 2000), 130) 
-        obast3 = choice(obstacles) 
+        obs3 = (rnd(1700, 2000), 130)
+        obast3 = choice(obstacles)
         if obast3 in [obstacle4, obstacle5, obstacle6]:obs3 = (obs3[0], 115)
     player_stading_cub = (5, height, 5+43,height+46)
     if height< 100:
-        start=True
+        if(not crashed):
+            start=True
     if start:
         obs1 = (obs1[0]-speed, obs1[1])
         obs2 = (obs2[0]-speed, obs2[1])
@@ -187,21 +185,17 @@ while not crashed:
             if -bg[0]>=600:bg1 = (600,150)
 
         if obs1_cub[0]<=player_stading_cub[2]-10<=obs1_cub[2] and obs1_cub[1]<=player_stading_cub[3]-10<=obs1_cub[3]-5:
-            start=False
-            crashed=True
+            start = False
+            crashed = True
             state = player_frame_4
         if obs2_cub[0]<=player_stading_cub[2]-10<=obs2_cub[2] and obs2_cub[1]<=player_stading_cub[3]-10<=obs2_cub[3]-5:
-            start=False
+            start = False
             crashed = True
             state = player_frame_4
         if obs3_cub[0]<=player_stading_cub[2]-10<=obs3_cub[2] and obs3_cub[1]<=player_stading_cub[3]-10<=obs3_cub[3]-5:
-            start=False
+            start = False
             crashed = True
             state = player_frame_4
     pygame.display.update()
     clock.tick(120)
-    tscore += 1
-    if start and not crashed and tscore % 12 == 0:
-        score += 1
-    if crashed:
-        print(score)
+    frameCounter += 1
